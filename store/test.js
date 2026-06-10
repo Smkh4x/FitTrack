7903b9c197d9576d74241fd45e12e9a5ddf00a7a
Smk777 <youssefalmouna8@gmail.com>
2026-06-10 18:09:09 +0200

    update project

---
+++ store/test.js
@@ -0,0 +1,13 @@
+let arr = [1 , 2 , 4 , 6 , 8]
+let res = [];
+
+for(let i = 0 ; i < arr.length ; i++){
+    if(i === arr[0]){
+console.log(i)
+    } else {
+        console.log("err")
+    }
+        
+    
+    
+}
